ccb8cf829723ac3da50dcd920f34fd3fe8262925
diegotheodoro <theodoro.diego@gmail.com>
2022-09-19 21:53:14 -0300

    Configuração do Ambiente

---
+++ src/main/java/br/com/oticanewlook/oticanewlook/OticanewlookApplication.java
@@ -0,0 +1,13 @@
+package br.com.oticanewlook.oticanewlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OticanewlookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OticanewlookApplication.class, args);
+	}
+
+}
